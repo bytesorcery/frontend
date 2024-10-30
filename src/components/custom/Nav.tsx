c3621e07fc25fb1f89a8a4282e94aa8494ad30f0
jimothy <timothynewton1998@gmail.com>
2024-10-30 17:34:11 -0400

    Added wrapper for nav bar - centered bar

--- src/components/custom/Nav.tsx
+++ src/components/custom/Nav.tsx
@@ -11,29 +11,33 @@ import {
 } from "@/components/ui/navigation-menu"
 import { Link } from "react-router-dom"
 import logo from "@/assets/byte-sorcery-logo-transparent.png"
-
+import "@/css/Nav.css"
 
 export default function Nav() {
     return (
-        <NavigationMenu className="fixed top-0 left-0 right-0 flex items-center justify-between max-w-full px-6 bg-background">
-            <img src={logo} alt="logo" width="100" />
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link to="/">Home</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link to="/about">About</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link to="/contact">Contact</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <div className="nav w-full fixed top-0 left-0 right-0 bg-background">
+            <NavigationMenu className="flex items-center px-6 mx-auto nav-menu">
+                <img src={logo} alt="logo" width="100" className="" />
+                <NavigationMenuList className="flex-1 flex justify-center">
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link to="/">Home</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link to="/pricing ">Pricing</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={`${navigationMenuTriggerStyle()} bg-accent text-primary-foreground`}>
+                            <Link to="/contact">Contact</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     )
 }
