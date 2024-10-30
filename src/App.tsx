f24361a691b3dcf5fe5f01941a41f4e32332e18f
jimothy <timothynewton1998@gmail.com>
2024-10-30 17:33:21 -0400

    Added next-themes for its ThemeProvider (control website themes)

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import './App.css'
 import Nav from '@/components/custom/Nav'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '@/components/pages/Home'
-import About from '@/components/pages/About'
 import Contact from '@/components/pages/Contact'
+import Pricing from '@/components/pages/Pricing'
+import { ThemeProvider } from 'next-themes'
 
 function App() {
 
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
