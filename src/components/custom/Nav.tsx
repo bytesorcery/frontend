9859300f64290819a93fdbcf5121ad80674f3350
jimothy <timothynewton1998@gmail.com>
2024-10-31 08:33:56 -0400

    got rid of unused imports - added scroll event listener

--- src/components/custom/Nav.tsx
+++ src/components/custom/Nav.tsx
@@ -1,22 +1,29 @@
 import {
     NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
     navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu"
 import { Link } from "react-router-dom"
 import logo from "@/assets/byte-sorcery-logo-transparent.png"
 import "@/css/Nav.css"
+import { useEffect, useState } from "react";
 
 export default function Nav() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const scrollThreshold = 0;
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > scrollThreshold);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className="nav w-full fixed top-0 left-0 right-0 bg-background">
-            <NavigationMenu className="flex items-center px-6 mx-auto nav-menu">
+        <div className="nav w-full fixed top-0 left-0 right-0 bg-transparent">
+            <NavigationMenu className={`flex items-center px-6 mx-auto nav-menu border-radius-10 ${isScrolled ? "nav-menu-scrolled" : ""}`}>
                 <img src={logo} alt="logo" width="100" className="" />
                 <NavigationMenuList className="flex-1 flex justify-center">
                     <NavigationMenuItem>
@@ -29,8 +36,6 @@ export default function Nav() {
                             <Link to="/pricing ">Pricing</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
-                </NavigationMenuList>
-                <NavigationMenuList>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild className={`${navigationMenuTriggerStyle()} bg-accent text-primary-foreground`}>
                             <Link to="/contact">Contact</Link>
